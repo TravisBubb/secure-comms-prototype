d53f27b497752f13996cd8762aedee7e1e1382ed
Travis Bubb <travisbubb14@gmail.com>
2025-10-06 22:34:58 -0400

    feat(lora): bump driver version and add esp log function for the driver log callback

--- main/main.c
+++ main/main.c
@@ -2,14 +2,33 @@
 #include "esp_system.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include <sx126x/sx126x.h>
 #include <stdio.h>
+#include <sx126x/sx126x.h>
+
+static void esp_log_wrapper(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+
+    char buffer[128];
+    vsnprintf(buffer, sizeof(buffer), fmt, args);
+    ESP_LOGI("SX126X", "%s", buffer);
+
+    va_end(args);
+}
 
 void app_main(void)
 {
   printf("Hello, ESP32-S3!\n");
 
   ESP_LOGI("Hello", "Hello from ESP-IDF!");
+
+  sx126x_config_t config = {
+      .log = esp_log_wrapper,
+      .frequency_hz = 915000000, // 915 MHz
+  };
+
+  sx126x_init(NULL, NULL, &config);
 
   while (1)
   {
