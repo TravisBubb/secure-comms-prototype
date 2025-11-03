4fc718a7f6740eec736d34ec9a340edee56d4390
Travis Bubb <travisbubb14@gmail.com>
2025-11-02 20:28:03 -0500

    feat(shell): set up secondary software serial channel using newly soldered header pins

--- secure-comms-prototype/src/main.cpp
+++ secure-comms-prototype/src/main.cpp
@@ -1,16 +1,21 @@
+#include "SoftwareSerial.h"
 #include "heltec_wifi_lora_v3.h"
 #include <Arduino.h>
 #include <RadioLib.h>
 
 SPIClass spiLoRa(HSPI);
 SX1262 radio = new Module(PIN_LORA_NSS, PIN_LORA_DIO1, PIN_LORA_RST, PIN_LORA_BUSY, spiLoRa);
+SoftwareSerial shellSerial(PIN_SOFTWARE_RX, PIN_SOFTWARE_TX);
 
 void setup()
 {
   Serial.begin(115200);
-  while (!Serial.available()) {}
+  while (!Serial) {}
+  Serial.println("USB Serial interface initialized.");
 
-  Serial.println("Serial interface initialized.");
+  shellSerial.begin(115200);
+  while (!shellSerial) {}
+  Serial2.println("Shell interface initialized.");
 
   Serial.println("Initializing SPI...");
   spiLoRa.begin(PIN_LORA_SCK, PIN_LORA_MISO, PIN_LORA_MOSI, PIN_LORA_NSS);
@@ -24,14 +29,26 @@ void setup()
                           LORA_SYNCWORD,
                           LORA_POWER_DBM,
                           LORA_PREAMBLE_LENGTH);
-  if (state == RADIOLIB_ERR_NONE)
-    Serial.println("SX1262 initialized.");
-  else
+  if (state != RADIOLIB_ERR_NONE)
   {
     Serial.print("SX1262 initialization failed, code: ");
     Serial.println(state);
     while (true) {}
   }
+
+  Serial.println("SX1262 initialized.");
 }
 
-void loop() {}
+void loop()
+{
+  while (shellSerial.available())
+  {
+    char c = shellSerial.read();
+    Serial.write(c);
+  }
+  while (Serial.available())
+  {
+    char c = Serial.read();
+    shellSerial.write(c);
+  }
+}
